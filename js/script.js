458bdfe6041ff79c651b8c30575fc7fe75b74783
Ramunas <rambek@gmail.com>
2021-01-26 14:11:19 +0200

    slider - version 1

--- js/script.js
+++ js/script.js
@@ -299,5 +299,48 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModalWindow();
         }, 4000);
     }
+
+    // POST Data from forms END
+    // Slider START
+
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+    
+    let slideIndex = 1;
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        } else if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(slide => slide.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+        if (slideIndex < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function changeSlide(n) {
+        showSlides(slideIndex += n);
+    }
+    showSlides(slideIndex);
+    prev.addEventListener('click', () => {
+        changeSlide(-1);
+    });
+    next.addEventListener('click', () => {
+        changeSlide(1);
+    });
 });
 
